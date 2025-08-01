b8c9be1ff8b88ca4b4b2aaa3862b4c691fb43e99
morysek <moravec.galaxy@seznam.cz>
2025-08-01 12:51:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -248,4 +248,17 @@ function ThreeDCar() {
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      background: "#000",
+      overflow: "hidden",
+      fontFamily: "'Inconsolata', monospace"
+    }}>
+      <TopBar />
+      <ThreeDCar />
+    </div>
+  );
+}
