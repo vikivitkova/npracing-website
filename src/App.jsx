0d3a0439026ac38d1702a19ed0680feb9fdc91de
morysek <moravec.galaxy@seznam.cz>
2025-08-01 10:33:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState, Suspense, useRef } from "react";
 import * as THREE from "three";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls, Environment, Center } from "@react-three/drei";
+import { OrbitControls, Environment, Center, useHelper } from "@react-three/drei";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 // NP Racing SVG Logo (original colors, scaled 20% smaller)
@@ -58,8 +58,7 @@ function NPLogo({ size = 336 }) {
             fill: "#fff",
             strokeLinejoin: "round",
           }}
-          d="m 111.60859,130.25083 c -0.96683,0.005 -1.91905,0.53479 -2.3828,1.51567 L 101.76888,147.53246 100,151.27435 h 5.85287 l 0.69867,-1.47846 h 5.2e-4 l 5.77949,-12.22045 11.88247,12.88242 c 1.27166,1.38021 3.53608,1.03468 4.33824,-0.66197 l 6.74016,-14.25185 h 16.1463 l -2.44895,5.17747 h -8.20725 l -2.50217,5.29115 h 12.38477 c 1.02253,-1.7e-4 1.95344,-0.58946 2.39107,-1.51361 l 4.95267,-10.46861 c 0.83036,-1.75547 -0.45016,-3.77762 -2.3921,-3.77755 h -21.99814 c -1.02309,-4.3e-4 -1.95475,0.58896 -2.39262,1.51361 l -5.7795,12.22096 -11.88247,-12.88294 c -0.53648,-0.58227 -1.24991,-0.85758 -1.95544,-0.85369 z"
-        />
+          d="m 111.60859,130.25083 c -0.96683,0.005 -1.91905,0.53479 -2.3828,1.51567 L 101.76888,147.53246 100,151.27435 h 5.85287 l 0.69867,-1.47846 h 5.2e-4 l 5.77949,-12.22045 11.88247,12.88242 c 1.27166,1.38021 3.53608,1.03468 4.33824,-0.66197 l 6.74016,-14.25185 h 16.1463 l -2.44895,5.17747 h -8.20725 l -2.50217,5.29115 h 12.38477 c 1.02253,-1.7e-4 1.95344,-0.58946 2.39107,-1.51361 l 4.95267,-10.46861 c 0.83036,-1.75547 -0.45016,-3.77762 -2.3921,-3.77755 h -21.99814 c -1.02309,-4.3e-4 -1.95475,0.58896 -2.39262,1.51361 l -5.7795,12.22096 -11.88247,-12.88294 c -0.53648,-0.58227 -1.24991,-0.85758 -1.95544,-0.85369 z" />
       </g>
     </svg>
   );
@@ -151,10 +150,11 @@ function LoadingScreen({ visible }) {
 function FloatingObjModel({ onLoad }) {
   const obj = useLoader(
     OBJLoader,
-    "/models/F1.obj",
+    "/models/car.obj", // ensure your file is named car.obj in public/models
     undefined,
-    () => onLoad?.()
+    onLoad
   );
+
   useEffect(() => {
     obj.traverse((child) => {
       if (child.isMesh) {
@@ -165,10 +165,11 @@ function FloatingObjModel({ onLoad }) {
       }
     });
   }, [obj]);
+
   return (
     <primitive
       object={obj}
-      scale={1}
+      scale={600000}
       position={[0, 0, 0.5]}
       castShadow
       receiveShadow
@@ -186,6 +187,24 @@ function ShadowPlane() {
   );
 }
 
+// Directional light with helper
+function DirLightWithHelper() {
+  const lightRef = useRef();
+  useHelper(lightRef, THREE.DirectionalLightHelper, 50000, 0xff0000);
+  return (
+    <directionalLight
+      ref={lightRef}
+      castShadow
+      intensity={2}
+      position={[0, 150000, 150000]}
+      shadow-mapSize-width={2048}
+      shadow-mapSize-height={2048}
+      shadow-bias={-0.0005}
+      shadow-normalBias={0.1}
+    />
+  );
+}
+
 // Main 3D component
 function ThreeDCar() {
   const [loading, setLoading] = useState(true);
@@ -200,9 +219,9 @@ function ThreeDCar() {
 
   return (
     <div
-      style={{  
+      style={{
         position: "relative",
-        marginTop: 90,               
+        marginTop: 90,
         width: "100%",
         height: "calc(100vh - 90px)",
         background: "#000",
@@ -211,68 +230,47 @@ function ThreeDCar() {
       <LoadingScreen visible={loading} />
 
       <Canvas
-  shadows
-  gl={{
-    antialias: true,
-    toneMapping: THREE.ACESFilmicToneMapping,
-    outputColorSpace: THREE.SRGBColorSpace,
-  }}
-  camera={{ position: [0, 0, 200000], fov: 7, near: 10000, far: 500000 }}
-  style={{ background: "#000", width: "100%", height: "100%" }}
-  onCreated={({ gl, scene }) => {
-    gl.shadowMap.enabled = true;
-    gl.shadowMap.type = THREE.PCFSoftShadowMap;
-    gl.toneMappingExposure = 1.25;
+        shadows
+        gl={{
+          antialias: true,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          outputColorSpace: THREE.SRGBColorSpace,
+        }}
+        camera={{ position: [0, 0, 200000], fov: 7, near: 10000, far: 500000 }}
+        style={{ background: "#000", width: "100%", height: "100%" }}
+        onCreated={({ gl, scene }) => {
+          gl.shadowMap.enabled = true;
+          gl.shadowMap.type = THREE.PCFSoftShadowMap;
+          gl.toneMappingExposure = 1.25;
+          scene.add(new THREE.AxesHelper(50000));
+        }}
+      >
+        <ambientLight intensity={0.3} />
+        <hemisphereLight skyColor={0xffffff} groundColor={0x222222} intensity={0.4} />
 
-    // Add a small axes helper at the origin so you can see the pivot
-    const axes = new THREE.AxesHelper(50000);
-    scene.add(axes);
-  }}
->
-  {/* fill so nothing is totally black */}
-  <ambientLight intensity={0.3} />
+        <DirLightWithHelper />
 
-  {/* sky/ground bounce */}
-  <hemisphereLight intensity={0.4} />
+        <Suspense fallback={null}>
+          <Environment preset="city" background />
+        </Suspense>
 
-  {/* repositioned directional light */}
-  <directionalLight
-    castShadow
-    intensity={2}
-    color={0xffffff}
-    // moved to camera’s upper‐right, pointing back at origin
-    position={[0, 150000, 150000]}
-    target-position={[0, 0, 0]}
-    shadow-mapSize-width={2048}
-    shadow-mapSize-height={2048}
-    shadow-bias={-0.0005}
-    shadow-normalBias={0.1}
-  />
+        <Suspense fallback={null}>
+          <Center>
+            <FloatingObjModel onLoad={() => setLoading(false)} />
+            <ShadowPlane />
+          </Center>
+        </Suspense>
 
-  {/* show the light’s helper so you can see where it is */}
-  <directionalLightHelper args={[ /* light ref will auto attach */ , 50000]} />
-
-  <Suspense fallback={null}>
-    <Environment preset="city" background />
-  </Suspense>
-
-  <Suspense fallback={null}>
-    <Center>
-      <FloatingObjModel onLoad={() => setLoading(false)} />
-      <ShadowPlane />
-    </Center>
-  </Suspense>
-
-  <OrbitControls
-    enablePan={false}
-    enableZoom={false}
-    enableDamping
-    dampingFactor={0.1}
-    target={[0, 0, 0]}
-    minPolarAngle={0.2}
-    maxPolarAngle={Math.PI - 0.2}
-  />
-</Canvas>
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          enableDamping
+          dampingFactor={0.1}
+          target={[0, 0, 0]}
+          minPolarAngle={0.2}
+          maxPolarAngle={Math.PI - 0.2}
+        />
+      </Canvas>
     </div>
   );
 }
@@ -294,3 +292,4 @@ export default function App() {
     </div>
   );
 }
+
