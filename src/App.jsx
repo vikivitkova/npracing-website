8f82c8fe639a7d6526b8de40b53cf7919e581d9e
morysek <moravec.galaxy@seznam.cz>
2025-08-01 14:53:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function NPLogo({ size = 300 }) {
     <svg
       src="/npracing.svg"
       alt="NP Racing Logo"
-      width="100%"
-      height="100%"
+      width={size}
+      height={(size * 30.96) / 104.14}
+      xmlns="http://www.w3.org/2000/svg"
       style={{ display: "block" }}>
     </svg>
   );
