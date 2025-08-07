128e09cf73fc616a90342aec89966200cfe3481d
morysek <moravec.galaxy@seznam.cz>
2025-08-07 21:59:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-  include: ["@react-three/postprocessing"]
-  },
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    open: true,
-  },
-  base: "/", // for GitHub Pages
-});
